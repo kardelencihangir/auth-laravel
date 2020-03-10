ee771de1f600dc922ac367e13cbcb67fee3980be
Kardelen Cihangir <kardelencihangir@gmail.com>
2020-03-10 10:23:01 +0300

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth'); //this is how we confirm authentication
+                                    //bunu ayrıca rota verirken de middleware tanımlayarak yapabiliriz.
+                                    //name altına ->middleware('auth') yazarak.
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
